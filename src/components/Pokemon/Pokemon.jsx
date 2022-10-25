f01d77062914daf69d793d7d4ecd103c55ed202c
Tiffany <hey@tiffanyrwhite.com>
2022-10-24 23:01:36 -0400

    Create shape of app

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -1,3 +1,5 @@
+// noinspection JSCheckFunctionSignatures
+
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import '../../styles/Pokemon.css'
@@ -5,22 +7,19 @@ import '../../styles/Pokemon.css'
 const Pokemon = () => {
   const [pokemon, setPokemon] = useState({});
   const [favorited, setFavorite] = useState(true);
-  const [image, setImage] = useState("");
-  const [pokeName, setPokeName] = useState("");
+  const [pokedex, setPokedex] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const API_URL = "https://pokeapi.co/api/v2/pokemon";
+
+  const API_URL = "https://pokeapi.co/api/v2/pokemon/?limit=20"
   useEffect(() => {
-    getPokemon();
-  }, []);
+    setLoading(true);
+    axios.get(API_URL).then(res => {
+      setLoading(false);
+      setPokemon(res.data.results);
+    });
+  }, [API_URL]);
 
-  const getPokemon = (id) => {
-    axios
-        .get(`${API_URL}/${id}`)
-        .then((response) => {
-      console.log(response.data)
-      setPokemon(response.data)
-    })
-  }
   const sortPokemon = (select) => {
     const options = {
       "a-z": [...pokemon].sort((a, b) => (a < b ? -1 : 1)),
@@ -28,29 +27,60 @@ const Pokemon = () => {
     };
 
     setPokemon(options[select.target.value]);
+
   };
+
+
 
   const handleChange = (e) => {
     setFavorite(e.target.checked);
   }
+  if (loading) return 'Loading...'
 
   return (
       <div>
-        <select name="sort A-Z" id="sort">
-        </select>
-        <h1>Pokedex</h1>
+        <div>
+          <select onChange={sortPokemon}>
+            <option value="a-z">A - Z</option>
+            <option value="z-a">Z - A</option>
+          </select>
+        </div>
         <div className="pokecard-container">
-          {pokemonCards.map((pokemon) => (
-              <div className="pokecard" key={pokemon.id}>
-                <h3>{pokemon.name}</h3>
-                <img src={pokemon.sprites.front_default}/>
-                <p>pokemon.abilities.ability.name</p>
-                <input type="checkbox" aria-label={}/>
-              </div>
-          ))}
+          <header>
+            <h1 className="title">Pokemon App</h1>
+          </header>
+
+          <section className="pokecards">
+            <h2>Find your favorite Pokemon!</h2>
+            <h3>{pokemon.name}</h3>
+          </section>
+
+          <section className="pokedex">
+            <h2>Gotta check 'em all!</h2>
+            <div className="pokelist" key={pokemon.id}>
+              {pokedex.map((pokemon) => (
+                  <div className="pokecard">
+                    <h3>{pokemon.name}</h3>
+                    <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon" + pokemon.id + ".png"} alt="pokemon image"/>
+                    <label>
+                      <input
+                          type="checkbox"
+                          checked={favorited}
+                          onChange={handleChange}
+                      />
+                      Favorite
+                    </label>
+                    <p>You {favorited ? 'liked' : 'did not like'} this.</p>
+                  </div>
+              ))}
+            </div>
+          </section>
+
         </div>
       </div>
   );
 }
+
+
 
 export default Pokemon;
